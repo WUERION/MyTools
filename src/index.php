89a608ff6b544e221dc07328fbdb1ea2d281b5d0
wuerion <almerux@gmail.com>
2024-08-02 20:11:29 -0600

    first commit creation of the documents

---
+++ src/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Tools</title>
+    <link rel="stylesheet" href="../style/output.css">
+    <link rel="shortcut icon" href="../img/logo.webp" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=MuseoModerno:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+
+</body>
+
+</html>
